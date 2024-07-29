190a9948daca5bff6b7e78dbd85da94d814e3598
Gabriel Rodrigues <90469897+GabrielEstmrLB@users.noreply.github.com>
2024-07-28 22:38:30 -0300

    add

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-general-ms
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_general_ms/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-general-ms.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-general-ms.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
